c74abe94fdbc94f6ef9ed5407603cc4125dd4bc4
Óscar Rivas <oscar.rivas.99@gmail.com>
2022-09-30 12:21:24 +0200

    clean index

--- index.php
+++ index.php
@@ -4,35 +4,29 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
-  <link rel="icon" type="image/png" href="./assets/img/favicon.png" />
-  <title>
-    EstovaEnserio
-  </title>
-  <!--     Fonts and icons     -->
+  <link rel="icon" type="image/png" href="./img/logo/favicon.ico" />
+  <title>EstovaEnserio</title>
 
   <!-- Nucleo Icons -->
   <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
   <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-
-  <!-- Material Icons -->
 
   <!-- CSS Files -->
   <link href="./assets/css/enserio-styles.css" rel="stylesheet" />
+  <link href="./assets/css/project-gallery.css" rel="stylesheet" />
 </head>
 
-<body class="bg-gray-200">
+<body>
   <!-- Navbar -->
-  <div class="position-sticky z-index-sticky top-0">
-    <!-- Navbar Light -->
-    <?php include_once('./includes/header.php'); ?>
-    <!-- End Navbar -->
+  <?php include_once('./includes/header.php'); ?>
+  <div class="blur shadow-blur pagina-listado-proyectos min-vh-80">
+    <!-- -------   START CONTENT 1 -------- -->
+    <!-- -------   END CONTENT  -------- -->
   </div>
-  <footer class="footer pt-5 mt-5"></footer>
-  <!--   Core JS Files   -->
-  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+
+  <!-- Footer -->
+  <?php include_once('./includes/footer.php'); ?>
+
 </body>
 
 </html>
