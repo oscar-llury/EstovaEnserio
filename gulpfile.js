fb33f49715daaffa6a4fc49980baa8d095da1e5d
Óscar Rivas <oscar.rivas.99@gmail.com>
2022-10-14 14:17:52 +0200

    upgrade CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-var gulp = require("gulp");
-var path = require("path");
-var sass = require("gulp-sass")(require("sass"));
-var autoprefixer = require("gulp-autoprefixer");
-var sourcemaps = require("gulp-sourcemaps");
-var open = require("gulp-open");
+const gulp = require("gulp");
+const path = require("path");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const open = require("gulp-open");
+const util = require("gulp-util");
+const minifyCSS = require("gulp-clean-css");
+const gulpif = require("gulp-if");
+const autoprefixer = require("gulp-autoprefixer");
+const log = require("fancy-log");
 
 var Paths = {
   HERE: "./",
@@ -13,8 +17,26 @@ var Paths = {
   SCSS: "./assets/scss/**/**",
 };
 
+// commando con --production para prod.
+var config = {
+  production: !!util.env.production,
+  develop: !util.env.production,
+  path: util.env.path || Paths.CSS,
+  scss: util.env.scss || Paths.SCSS_TOOLKIT_SOURCES,
+};
+
 gulp.task("compile-scss", function () {
-  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES).pipe(sourcemaps.init()).pipe(sass().on("error", sass.logError)).pipe(autoprefixer()).pipe(sourcemaps.write(Paths.HERE)).pipe(gulp.dest(Paths.CSS));
+  log("working browsersync -> " + config.scss);
+  log(config.develop);
+  log(config.production);
+  return gulp
+    .src(config.scss)
+    .pipe(gulpif(config.develop, sourcemaps.init()))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(config.production ? minifyCSS() : util.noop())
+    .pipe(autoprefixer())
+    .pipe(gulpif(config.develop, sourcemaps.write(".")))
+    .pipe(gulp.dest(config.path));
 });
 
 gulp.task("watch", function () {
