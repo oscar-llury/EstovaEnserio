78029df5990f82baebbb2fb95fb6491c6989d552
Óscar Rivas <oscar.rivas.99@gmail.com>
2022-10-28 12:24:02 +0200

    diseño de pagina para proyecto

--- contenido-digital/lio-studio.php
+++ contenido-digital/lio-studio.php
@@ -4,42 +4,70 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png" />
-  <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
-  <title>
-    EstovaEnserio
-  </title>
-  <!--     Fonts and icons     -->
+  <link rel="icon" type="image/png" href="./img/logo/favicon.ico" />
+  <title>EstovaEnserio</title>
 
   <!-- Nucleo Icons -->
-  <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+
   <!-- Font Awesome Icons -->
   <link href="/node_modules/@fortawesome/fontawesome-free/css/fontawesome.css" rel="stylesheet">
   <link href="/node_modules/@fortawesome/fontawesome-free/css/brands.css" rel="stylesheet">
   <link href="/node_modules/@fortawesome/fontawesome-free/css/solid.css" rel="stylesheet">
+
   <!-- CSS Files -->
-  <link href="/assets/css/enserio-styles.css" rel="stylesheet" />
+  <link href="../assets/css/enserio-styles.css" rel="stylesheet" />
+
 </head>
 
-<body class="bg-gray-200">
+<body class="bg-gray-200 pagina-proyecto">
   <!-- Navbar -->
   <?php include_once('../includes/header.php'); ?>
   <div class="blur shadow-blur">
     <!-- -------   START CONTENT 1 - title & description and 6 IMAGES   -------- -->
-    <div class="container-fluid">
-      <div class="position-relative img-header d-flex align-items-center">
-        <h1 class="position-absolute">Lio Studio</h1>
-        <img src="../img/background-image-1.jpg" alt="" class="w-100" />
+    <div class="container-fluid img-header position-relative d-flex justify-content-center">
+      <div class="main-title position-absolute container">
+        <h1 class="">Lio Studio</h1>
       </div>
+      <img src="../img/background-image-1.jpg" alt="" class="w-100 object-fit-cover" />
+    </div>
+    <div class="container-fluid">
       <div class="py-5 main-container">
         <div class="container">
           <div class="row">
             <div class="col-4 col-info-proyecto">
-              <h2 class="line-after">PLEASANT BALANCE</h2>
-              <p>Alienum phaedrum torquatos nec eu, vis det raxit periculis ex, nihil expetendis in mei. Mei an pericula euripidis, hinc partem ei est. Eos ei nisl graecis, vix aperiri consequat. In ligula et augue pretium ultrices.</p>
-              <button class="btn btn-primary" type="button">Button</button>
-              <p class="title-section">Info</p>
+              <h2 class="line-after">Lio Studio</h2>
+              <p>Creación de contenido digital más base de website para 710 studio desde sus inicios, con fotografía y vídeo para ecommerce y campañas de moda.</p>
+              <div class="accordion my-4" id="accordionDescripcion">
+                <div class="accordion-item">
+                  <div class="accordion-header" id="dscrheader">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dscrcollapse" aria-expanded="false" aria-controls="dscrcollapse">
+                      Descripción
+                    </button>
+                  </div>
+                  <div id="dscrcollapse" class="accordion-collapse collapse" aria-labelledby="dscrheader" data-bs-parent="#accordionDescripcion">
+                    <div class="accordion-body">
+                      <p>Marca de prendas polivalentes. Son una marca que apuesta por la sostenibilidad, y con tejidos Pyratex. Comprometidas con el medio ambiente, 710 Studio trabaja con materiales reciclados, además de ello suman un QR a sus prendas el cual se activa cuando la prenda comienza a ser perjudicial para el medioambiente. Así el consumidor puede devolver la prenda a cambio de descuentos y el tejido volverá a formar parte de la cadena de producción.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="accordion my-4" id="accordionMakingoff">
+                <div class="accordion-item">
+                  <div class="accordion-header" id="mkgoffheader">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#mkgoffcollapse" aria-expanded="false" aria-controls="mkgoffcollapse">
+                      Making off
+                    </button>
+                  </div>
+                  <div id="mkgoffcollapse" class="accordion-collapse collapse" aria-labelledby="mkgoffheader" data-bs-parent="#accordionMakingoff">
+                    <div class="accordion-body">
+                      <p>Marca de prendas polivalentes. Son una marca que apuesta por la sostenibilidad, y con tejidos Pyratex. Comprometidas con el medio ambiente, 710 Studio trabaja con materiales reciclados, además de ello suman un QR a sus prendas el cual se activa cuando la prenda comienza a ser perjudicial para el medioambiente. Así el consumidor puede devolver la prenda a cambio de descuentos y el tejido volverá a formar parte de la cadena de producción.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="title-section mt-4">Info</div>
               <table class="table-info-proyecto">
                 <tbody>
                   <tr>
@@ -56,8 +84,8 @@
                   </tr>
                 </tbody>
               </table>
-              <span class="line-after"></span>
-              <div>
+
+              <div class="mt-4">
                 <p class="d-inline-block">Compartir:</p>
                 <button type="button" class="btn btn-icon-only rounded-circle text-lg">
                   <i class="fa-brands fa-facebook-f"></i>
@@ -74,7 +102,7 @@
               </div>
             </div>
             <div class="col-8">
-              <img src="../img/background-image-1.jpg" alt="" class="w-100" />
+              <img src="../img/contenido-digital/01-lio_studio.jpg" alt="" class="w-100" />
             </div>
           </div>
         </div>
