97808772950061f906ee676497a78608aebd81c1
Óscar Rivas <oscar.rivas.99@gmail.com>
2022-12-27 16:01:15 +0100

    created projects canvas in home

--- index.php
+++ index.php
@@ -11,31 +11,61 @@
 <body class="no-scoll-bar">
   <!-- Navbar -->
   <?php include_once('./includes/header.php'); ?>
-  <div class="main-content shadow-blur pagina-home h-100 vh-min-100">
+  <!--<div class="main-content shadow-blur pagina-home h-100 vh-min-100">-->
+  <div class="main-content shadow-blur pagina-home enserio-page">
     <!-- -------   START CONTENT 1 -------- -->
-    <div class="position-absolute start-50 top-50 big-container">
+
+    <div class="big-container">
       <div class="d-flex align-items-center justify-content-center">
         <h1 class="text-center m-0 title px-4 text-bg-dark">¡Hola!</h1>
       </div>
     </div>
-    <!-- -------   END CONTENT  -------- -->
-  </div>
+    <div class="container enserio-projects-canvas">
+      <div class="collection-list-wrapper-project w-dyn-list">
+        <div role="list" class="collection-list-project w-dyn-items">
+          <div role="listitem" class="collection-item-project w-dyn-item">
+            <a class="projects-grid-wrapper" href='#'>
+              <img src="https://assets.website-files.com/5f328817663c02c74c891a47/617ad2a77fda624fa0072e8e_Screenshot%202021-10-28%20at%2018.40.07.png" loading="lazy" alt="" class="main-img-project">
+              <h2 class="h3 title">Producciones</h2>
+            </a>
+          </div>
+          <div role="listitem" class="collection-item-project w-dyn-item">
+            <a class="projects-grid-wrapper" href='#'>
+              <img src="https://assets.website-files.com/5f328817663c02c74c891a47/6261ab63fd0603f9f29ac442_Screenshot%202022-04-21%20at%2021.06.11.png" loading="lazy" alt="" class="main-img-project">
+              <h2 class="h3 title">Eventos</h2>
+            </a>
+          </div>
+          <div role="listitem" class="collection-item-project w-dyn-item">
+            <a class="projects-grid-wrapper" href='#'>
+              <img src="https://assets.website-files.com/5f328817663c02c74c891a47/618c1e64cd9bce756fe9e6b8__TAN1245%20(1)%20(1).jpg" loading="lazy" alt="" class="main-img-project">
+              <h2 class="h3 title">Contenido digital</h2>
+            </a>
+          </div>
+          <div data-poster-url="https://assets.website-files.com/5f2b3234c316893b51b5e73c/5f89953b3bdee21ed1bf3181_lightbros-reel-poster-00001.jpg" data-video-urls="https://assets.website-files.com/5f2b3234c316893b51b5e73c/5f89953b3bdee21ed1bf3181_lightbros-reel-transcode.mp4,https://assets.website-files.com/5f2b3234c316893b51b5e73c/5f89953b3bdee21ed1bf3181_lightbros-reel-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video-projects"><video id="f03f0295-28fa-1c0c-ccd8-91bedb54842e-video" autoplay="" loop="" style="background-image:url(&quot;https://assets.website-files.com/5f2b3234c316893b51b5e73c/5f89953b3bdee21ed1bf3181_lightbros-reel-poster-00001.jpg&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover">
+              <source src="https://assets.website-files.com/5f2b3234c316893b51b5e73c/5f89953b3bdee21ed1bf3181_lightbros-reel-transcode.mp4" data-wf-ignore="true">
+              <source src="https://assets.website-files.com/5f2b3234c316893b51b5e73c/5f89953b3bdee21ed1bf3181_lightbros-reel-transcode.webm" data-wf-ignore="true">
+            </video></div>
+        </div>
+        <!-- -------   END CONTENT  -------- -->
+      </div>
 
-  <!-- Footer -->
-  <footer class="footer py-3 bg-white blur shadow position-absolute bottom-0 w-100">
-    <div class=" text-center">
-      <p class="m-0">&copy; Esto va Enserio S.L. <script>
-          document.write(new Date().getFullYear());
-        </script>. Todos los derechos reservados.</p>
-    </div>
-  </footer>
+      <!-- Footer -->
+      <?php include_once('./includes/footer.php'); ?>
+      <!--
+      <footer class="footer py-3 bg-white blur shadow position-absolute bottom-0 w-100">
+        <div class=" text-center">
+          <p class="m-0">&copy; Esto va Enserio S.L. <script>
+              document.write(new Date().getFullYear());
+            </script>. Todos los derechos reservados.</p>
+        </div>
+      </footer>-->
 
-  <!--   Core JS Files   -->
-  <script src="/assets/js/core/popper.min.js"></script>
-  <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+      <!--   Core JS Files   -->
+      <script src="/assets/js/core/popper.min.js"></script>
+      <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
 
-  <!-- Theme JS -->
-  <script src="./assets/js/material-kit-pro.min.js" type="text/javascript"></script>
+      <!-- Theme JS -->
+      <script src="./assets/js/material-kit-pro.min.js" type="text/javascript"></script>
 
 </body>
 
